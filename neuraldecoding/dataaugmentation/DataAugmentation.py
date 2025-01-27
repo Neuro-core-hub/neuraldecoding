4731781b1ee0209c091c0e11b4e11c23a312ee93
ChangLiu <98478845+ChangLiu2022@users.noreply.github.com>
2025-01-27 15:22:11 -0500

    Removed legacy functions. Resolved scipy/sklearn confusion. Added more detailed descriptions. Added requirements for data augmentation

--- neuraldecoding/dataaugmentation/DataAugmentation.py
+++ neuraldecoding/dataaugmentation/DataAugmentation.py
@@ -8,52 +8,6 @@ import sys
 EPSILON = sys.float_info.epsilon
 
 # ------------------------
-# --- Legacy Functions ---
-# ------------------------
-
-def add_training_noise(x,
-                       bias_neural_std=None,
-                       noise_neural_std=None,
-                       noise_neural_walk_std=None,
-                       bias_allchans_neural_std=None,
-                       device='cpu'):
-    """Function to add different types of noise to training input data to make models more robust.
-       Identical to the methods in Willet 2021.
-
-    Args:
-        x (tensor):                     neural data of shape [batch_size x num_chans x conv_size]
-        bias_neural_std (float):        std of bias noise
-        noise_neural_std (float):       std of white noise
-        noise_neural_walk_std (float):  std of random walk noise
-        bias_allchans_neural_std (float): std of bias noise, bias is same across all channels
-        device (device):                torch device (cpu or cuda)
-    """
-    if bias_neural_std:
-        # bias is constant across time (i.e. the 3 conv inputs), but different for each channel & batch
-        # biases = torch.normal(0, bias_neural_std, x.shape[:2]).unsqueeze(2).repeat(1, 1, x.shape[2])
-        biases = torch.normal(torch.zeros(x.shape[:2]), bias_neural_std).unsqueeze(2).repeat(1, 1, x.shape[2])
-        x = x + biases.to(device=device)
-
-    if noise_neural_std:
-        # adds white noise to each channel and timepoint (independent)
-        # noise = torch.normal(0, noise_neural_std, x.shape)
-        noise = torch.normal(torch.zeros_like(x), noise_neural_std)
-        x = x + noise.to(device=device)
-
-    if noise_neural_walk_std:
-        # adds a random walk to each channel (noise is summed across time)
-        # noise = torch.normal(0, noise_neural_walk_std, x.shape).cumsum(dim=2)
-        noise = torch.normal(torch.zeros_like(x), noise_neural_walk_std).cumsum(dim=2)
-        x = x + noise.to(device=device)
-
-    if bias_allchans_neural_std:
-        # bias is constant across time (i.e. the 3 conv inputs), and same for each channel
-        biases = torch.normal(torch.zeros((x.shape[0], 1, 1)), bias_allchans_neural_std).repeat(1, x.shape[1], x.shape[2])
-        x = x + biases.to(device=device)
-
-    return x
-
-# ------------------------
 # --- Helper Functions ---
 # ------------------------
 
@@ -236,7 +190,7 @@ def normalize_scaler(X,
 
         Args:
             X (ndarray): numpy array of size [N, numfeats]
-            Normalizer (scipy.scaler): sklearn scaler
+            Normalizer (sklearn.scaler): sklearn scaler with predefined parameters (e.g. sklearn.preprocessing.StandardScaler(with_mean=True, with_std=True))
         
         Returns:
             normalize_X (ndarray): Normalized input data
@@ -378,12 +332,12 @@ def normalize(X,
 
         Args:
             X (ndarray): numpy array of size [N, numfeats]
-            method (string): Method to use for normalization. Options includes 'moving_average', 'scipy'.
-            **kwargs: additional parameters. For moving average, it includes 'win_size', and 'axis'. For scipy, it includes 'normalizer'.
+            method (string): Method to use for normalization. Options includes 'moving_average', 'sklearn'.
+            **kwargs: additional parameters. For moving average, it includes 'win_size', and 'axis'. For sklearn, it includes 'normalizer'. N.B. Normalizer should contain predefined parameters (e.g. sklearn.preprocessing.StandardScaler(with_mean=True, with_std=True))
 
         Returns:
             normalize_X (ndarray): Normalized input data
-            normalizer (scipy.scaler): Scipy normalizer, none if method is moving average
+            normalizer (sklearn.scaler): Sklearn normalizer, none if method is moving average
     """
 
     if(method == "moving_average"):
@@ -393,10 +347,10 @@ def normalize(X,
             raise ValueError("moving_average method requires 'win_size' and 'axis'")
         normalize_X = normalize_moving_average(X, win_size, axis)
         return normalize_X, None
-    elif(method == "scipy"):
+    elif(method == "sklearn"):
         normalizer = kwargs.get('normalizer')
         if normalizer is None:
-            raise ValueError("scipy method requires 'normalizer'")
+            raise ValueError("sklearn method requires 'normalizer'")
         normalize_X, normalizer = normalize_scaler(X, normalizer)
         return normalize_X, normalizer
 
