a39cbe0765523272228ad0e33af7afb5c3214b58
Luis Cubillos <lhcubill@umich.edu>
2025-10-07 16:34:26 -0400

    somewhat working

--- neuraldecoding/preprocessing/preprocessing.py
+++ neuraldecoding/preprocessing/preprocessing.py
@@ -51,17 +51,18 @@ class OnlinePreprocessing(Preprocessing):
     def __init__(self, config):
         super().__init__(config)
     
-    def preprocess_pipeline(self, data, logger, publish_func, params = {'is_train': True}):
+    def preprocess_pipeline(self, data, params = {'is_online': True}, publish_intermediate_steps = False, publish_func = None):
         current_data = data
         inter_pipeline_data = {}
         inter_pipeline_data.update(params)
         for step in self.pipeline:
             step_instance = step['instance']
-            current_data, inter_pipeline_data = step_instance.transform(current_data, inter_pipeline_data) #TODO, change to the method that transforms online stuff
-            if self.is_debug:
-                logger.info(f"Publishing step {step['name']} and interpipe")
-                publish_func(
-                            f'preprocessing_{step['name']}',
-                            {'step': step['name'], 'data': current_data, 'interpipe': inter_pipeline_data}
-                        )
-        publish_func('preprocessing', {'step': step['name'], 'data': current_data, 'interpipe': inter_pipeline_data})# TODO: ,parents)
+            current_data, inter_pipeline_data = step_instance.transform_online(current_data, inter_pipeline_data)
+            if publish_intermediate_steps:
+                if publish_func is None:
+                    raise ValueError("publish_func is required when publish_intermediate_steps is True")
+                publish_func({'step': step['name'], 'data': current_data, 'interpipe': inter_pipeline_data}, is_intermediate_step = True, step_name=step["name"])
+        if publish_func is None:
+            raise ValueError("publish_func is required when publish_intermediate_steps is True")
+        publish_func(current_data, is_intermediate_step=False)
+        return current_data
